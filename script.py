06999d28493475fd0b72afd65b3d13229656c7f0
DonavanC13 <donavanc13@outlook.com>
2024-07-26 22:08:16 -0400

    added script.py file and filled it with some basic commands

---
+++ script.py
@@ -0,0 +1,11 @@
+print("Hello! What is your name?")
+
+name = "Donavan Curtis"
+print(f"Nice to meet you,{name}!")
+
+print("How old are you?")
+age = 22
+bot_age = 3
+age_difference = age - bot_age
+
+print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
