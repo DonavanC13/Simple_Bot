78e5b88320a862726ed273f293f0593efca42f6d
DonavanC13 <donavanc13@outlook.com>
2024-07-26 22:15:59 -0400

    Added basic conversation flow with user name, age, and favorite color

--- script.py
+++ script.py
@@ -9,3 +9,7 @@ bot_age = 3
 age_difference = age - bot_age
 
 print(f"You are {age_difference} years older than me. I'm only {bot_age} years old!")
+
+print("What's your favorite color?")
+color = "skyblue"
+print(f"Oh, {color} is a beautiful color!")
